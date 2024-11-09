5df35d47ceb5394a7a7b9ba4361a697c37d7f22c
naeem majumder <naeemmajumder00@gmail.com>
2024-11-09 12:56:15 +0600

    add weather and geocoding api

--- major-react-project/src/Components/SearchBox.jsx
+++ major-react-project/src/Components/SearchBox.jsx
@@ -6,6 +6,22 @@ import { useState } from 'react';
 export default function SearchBox(){
     let [city, setCity] = useState("");
 
+    
+    // API URL GeoCoding
+    const API_URL_GEOCODING = "https://api.openweathermap.org/data/2.5/weather";
+    const API_KEY = "e1cb4dab7116a71d8cfb74732a05182e";
+
+
+
+
+    let getWeatherInfo_GEOCODING = async()=>{
+        let response = await fetch(`${API_URL_GEOCODING}?q=${city}&appid=${API_KEY}&units=metric`);
+        let jsonResponse = await response.json();
+        console.log(jsonResponse);
+    }
+
+
+
 
     let handleChange = (event)=>{
         setCity(event.target.value);
@@ -14,7 +30,8 @@ export default function SearchBox(){
     let handleSubmit = (event)=>{
         event.preventDefault();
         console.log(city);
-        setCity("")
+        setCity("");
+        getWeatherInfo_GEOCODING();
     }
 
     return(
